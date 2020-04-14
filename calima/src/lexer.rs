bfea694221005427de5506106c356f1edecc3d66
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-04-14 19:01:56 +0200

    Make lexer discard whitespace and comments

--- calima/src/lexer.rs
+++ calima/src/lexer.rs
@@ -1,5 +1,6 @@
 use std::str::Chars;
 use crate::token::{Token, Location};
+use crate::lexer::ErrorKind::TabIndent;
 
 pub struct Lexer<'input> {
     chars: Chars<'input>,
@@ -37,5 +38,47 @@ impl<'input> Lexer<'input> {
         self.col = 1;
         self.line += 1;
         self.pos += 1;
+    }
+
+    fn comment(&mut self) {
+        loop {
+            match self.chars.next() {
+                None => break,
+                Some('\n') => {
+                    self.incr_line();
+                    break;
+                }
+                Some(_) => self.incr_pos()
+            }
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub enum ErrorKind {
+    TabIndent
+}
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub struct Error {
+    location: Location,
+    kind: ErrorKind
+}
+
+impl<'input> Iterator for Lexer<'input> {
+    type Item = Result<(Location, Token<'input>, Location), Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            match self.chars.next() {
+                None => break None,
+                Some(' ') => self.incr_pos(),
+                Some('\n') => self.incr_line(),
+                Some('\r') => self.incr_pos(),
+                Some('\t') => break Some(Err(Error { location: self.current_pos(), kind: TabIndent })),
+                Some('#') => self.comment(),
+                Some(_) => ()
+            }
+        }
     }
 }
