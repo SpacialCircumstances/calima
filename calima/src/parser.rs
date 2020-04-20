10f9dd55cfba89f6722838be1a50006ca855bda1
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-04-20 19:31:32 +0200

    Parse blocks and do statement

--- calima/src/parser.rs
+++ calima/src/parser.rs
@@ -5,7 +5,7 @@ lalrpop_mod!(pub calima_parser);
 
 pub fn parse<'a>(code: &'a str) -> Result<Block<'a, ()>, String> {
     let lexer = Lexer::new(code);
-    let parser = calima_parser::ProgramParser::new();
-    let statements = parser.parse(code, lexer);
+    let parser = calima_parser::BlockParser::new();
+    let block = parser.parse(code, lexer);
     Err(String::from("Error"))
 }
