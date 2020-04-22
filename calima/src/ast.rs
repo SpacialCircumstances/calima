7b66cdb88817a8ce0356bd38df4a245cab546b83
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-04-22 17:57:18 +0200

    Parse case expression

--- calima/src/ast.rs
+++ calima/src/ast.rs
@@ -68,6 +68,6 @@ pub enum Expr<'a, Data> {
     Literal(Literal<'a>, Data),
     Lambda(Vec<Pattern<'a>>, Block<'a, Data>, Data),
     If { data: Data, cond: Box<Expr<'a, Data>>, if_true: Block<'a, Data>, if_false: Block<'a, Data> },
-    Case { value: Box<Expr<'a, Data>>, matches: Vec<(Pattern<'a>, Block<'a, Data>)> },
+    Case { data: Data, value: Box<Expr<'a, Data>>, matches: Vec<(Pattern<'a>, Block<'a, Data>)> },
     List(Vec<Expr<'a, Data>>, Data)
 }
