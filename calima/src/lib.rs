a2888a6c44ad66b2f481822fc38e503cc75988ad
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-04-22 20:01:13 +0200

    CLI compiler that prints parsed AST

--- calima/src/lib.rs
+++ calima/src/lib.rs
@@ -1,6 +1,18 @@
 #[macro_use] extern crate lalrpop_util;
 
+use std::fs::File;
+use std::io::Read;
+
 mod token;
 mod lexer;
 mod ast;
 mod parser;
+
+pub fn compile(file_name: &str) -> Result<(), String> {
+    let mut file = File::open(file_name).expect(format!("Error opening file {}", file_name).as_ref());
+    let mut file_content = String::new();
+    file.read_to_string(&mut file_content).expect("Error reading from input file");
+    let ast = parser::parse(&file_content)?;
+    println!("{:#?}", ast);
+    Ok(())
+}
--- calimac/src/main.rs
+++ calimac/src/main.rs
@@ -1,3 +1,14 @@
+use clap::Clap;
+use calima::compile;
+
+#[derive(Clap)]
+#[clap(version = "0.1.0", author = "SpacialCircumstances")]
+struct Options {
+    input_file: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    let options: Options = Options::parse();
+    let file_name = &options.input_file;
+    compile(file_name).expect("Compiler error");
 }
