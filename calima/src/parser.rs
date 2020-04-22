c68cb2f5e1e383005c0dc3d012db217c4d113092
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-04-22 18:28:59 +0200

    Make parser generic over data associated with AST

--- calima/src/parser.rs
+++ calima/src/parser.rs
@@ -7,7 +7,7 @@ lalrpop_mod!(pub calima_parser);
 pub fn parse<'a>(code: &'a str) -> Result<Block<'a, Location>, String> {
     let lexer = Lexer::new(code);
     let parser = calima_parser::BlockParser::new();
-    let block = parser.parse(code, lexer).map_err(|e| format!("{}", e))?;
+    let block = parser.parse(code, &|loc| loc, lexer).map_err(|e| format!("{}", e))?;
     Ok(block)
 }
 
