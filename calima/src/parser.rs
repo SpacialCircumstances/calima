cf0cea607939d095c7dc3bb5975140443cafe178
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-04-24 15:58:31 +0200

    Add test for hello world parsign

--- calima/src/parser.rs
+++ calima/src/parser.rs
@@ -14,12 +14,22 @@ pub fn parse<'a>(code: &'a str) -> Result<Block<'a, Location>, String> {
 #[cfg(test)]
 mod tests {
     use crate::parser::parse;
+    use crate::ast::{Block, Literal};
+    use crate::ast::Expr::*;
+    use crate::token::Location;
 
     #[test]
     fn test_hello_world() {
         let code = include_str!("test_snippets/hello_world.ca");
         let parsed = parse(code);
         assert!(parsed.is_ok());
-        println!("{:#?}", parsed.unwrap());
+        let ast = parsed.unwrap();
+        let expected = Block {
+            statements: Vec::new(),
+            result: Box::new(FunctionCall(Box::new(Variable(vec![ "println" ], Location { pos: 0, col: 1, line: 1 })), vec![
+                Literal(Literal::String("Hello World!"), Location { pos: 8, col: 9, line: 1 })
+            ], Location { pos: 0, col: 1, line: 1 }))
+        };
+        assert_eq!(ast, expected);
     }
 }
