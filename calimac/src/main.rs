01fc0f3ba6a09c1d9511a502eed922e36b2fc925
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-05-14 23:04:56 +0200

    Add cli option for module paths

--- calimac/src/main.rs
+++ calimac/src/main.rs
@@ -4,12 +4,15 @@ use calima::{compile, CompilerArguments};
 #[derive(Clap)]
 #[clap(version = "0.1.0", author = "SpacialCircumstances")]
 struct Options {
-    input_file: String
+    input_file: String,
+    #[clap(short, long)]
+    module_paths: Vec<String>
 }
 
 fn main() {
     let options: Options = Options::parse();
     let file_name = &options.input_file;
-    let args = CompilerArguments::new(file_name, Vec::new());
+    let module_paths = options.module_paths.iter().map(AsRef::as_ref).collect();
+    let args = CompilerArguments::new(file_name, module_paths);
     compile(args).expect("Compiler error");
 }
