4d6bd754771ddc0370d99c64a7f816dc9a51e087
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-06-25 22:39:19 +0200

    Store modules in typed context

--- calima/src/typechecker.rs
+++ calima/src/typechecker.rs
@@ -1,16 +1,24 @@
 use crate::compiler::ModuleTreeContext;
 use crate::errors::ErrorContext;
 use crate::string_interner::StringInterner;
-use crate::common::Module;
+use crate::common::{Module, ModuleIdentifier};
 use crate::token::Span;
+use std::collections::HashMap;
 
-pub struct TypedContext {
-
+pub struct TypeData {
+    position: Span
 }
 
-pub fn typecheck(string_interner: &StringInterner, errors: &mut ErrorContext, module_ctx: ModuleTreeContext) -> Result<TypedContext, ()> {
+pub struct TypedContext<'input> {
+    pub modules: HashMap<ModuleIdentifier, Module<'input, TypeData>>,
+}
+
+pub fn typecheck<'input>(string_interner: &StringInterner, errors: &mut ErrorContext, module_ctx: ModuleTreeContext<'input>) -> Result<TypedContext<'input>, ()> {
+    let mut ctx = TypedContext {
+        modules: HashMap::new()
+    };
     let mut orderered_modules: Vec<&Module<Span>> = module_ctx.modules.values().collect();
     orderered_modules.sort_by(|m1, m2| m1.depth.cmp(&m2.depth));
 
-    errors.handle_errors().map(|()| TypedContext {})
+    errors.handle_errors().map(|()| ctx)
 }
