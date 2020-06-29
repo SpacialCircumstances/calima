8a38155c88361527e70e5e809251767cde790b83
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-06-29 23:08:45 +0200

    Destructively typecheck modules to avoid cloning

--- calima/src/typechecker.rs
+++ calima/src/typechecker.rs
@@ -5,7 +5,7 @@ use crate::common::{Module, ModuleIdentifier};
 use crate::token::Span;
 use std::collections::HashMap;
 
-fn typecheck_module<'input>(unchecked: &Module<'input, Span>, deps: &[Module<'input, TypeData>]) -> Module<'input, TypeData> {
+fn typecheck_module<'input>(unchecked: Module<'input, Span>, deps: Vec<&Module<'input, TypeData>>) -> Module<'input, TypeData> {
     unimplemented!()
 }
 
@@ -17,16 +17,16 @@ pub struct TypedContext<'input> {
     pub modules: HashMap<ModuleIdentifier, Module<'input, TypeData>>,
 }
 
-pub fn typecheck<'input>(string_interner: &StringInterner, errors: &mut ErrorContext, module_ctx: ModuleTreeContext<'input>) -> Result<TypedContext<'input>, ()> {
+pub fn typecheck<'input>(string_interner: &StringInterner, errors: &mut ErrorContext, mut module_ctx: ModuleTreeContext<'input>) -> Result<TypedContext<'input>, ()> {
     let mut ctx = TypedContext {
         modules: HashMap::new()
     };
-    let mut ordered_modules: Vec<&Module<Span>> = module_ctx.modules.values().collect();
+    let mut ordered_modules: Vec<Module<Span>> = module_ctx.modules.drain().map(|(_, module)| module).collect();
     ordered_modules.sort_by(|m1, m2| m1.depth.cmp(&m2.depth));
 
     for module in ordered_modules {
         let deps = module.deps.iter().map(|(d, _)| ctx.modules.get(d).expect("Fatal error: Dependent module not found")).collect();
-        ctx.modules.insert(module.name.clone(), typecheck_module(module, deps))
+        ctx.modules.insert(module.name.clone(), typecheck_module(module, deps));
     }
 
     errors.handle_errors().map(|()| ctx)
