1162808525ff4a8588d4a0fd9644546bbec7bf25
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-09-17 00:16:09 +0200

    Add basic math operators to prelude

--- calima/src/prelude.rs
+++ calima/src/prelude.rs
@@ -1,5 +1,22 @@
-use crate::types::Exports;
+use crate::types::{Exports, Scheme, build_function, int, float};
+
+fn int_op() -> Scheme {
+    Scheme::simple(build_function(&[ int(), int() ], &int()))
+}
+
+fn float_op() -> Scheme {
+    Scheme::simple(build_function(&[ float(), float() ], &float()))
+}
 
 pub fn prelude() -> Exports<'static> {
-    Exports::new()
+    let mut ex = Exports::new();
+    ex.add_variable("+", int_op());
+    ex.add_variable(".+", float_op());
+    ex.add_variable("-", int_op());
+    ex.add_variable(".-", float_op());
+    ex.add_variable("*", int_op());
+    ex.add_variable(".*", float_op());
+    ex.add_variable("/", int_op());
+    ex.add_variable("./", float_op());
+    ex
 }
