34ded2ebf5ed907b8c12ff32187cc6bf2265ad33
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-10-03 15:50:39 +0200

    Fix borrow checker warning

--- calima/src/typechecker.rs
+++ calima/src/typechecker.rs
@@ -75,8 +75,9 @@ impl Context {
     }
 
     fn bind(&mut self, gid: GenericId, t2: &Type) {
-        match &self.subst[gid] {
-            Some(t) => self.unify(&t.clone(), t2),
+        let existing = self.subst[gid].clone();
+        match existing {
+            Some(t) => self.unify(&t, t2),
             None => self.subst.add(gid, t2.clone())
         }
     }
