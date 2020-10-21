700da6381b674f5539a72d9babf7ebcf2a141df2
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2020-10-21 23:27:52 +0200

    Correctly implement display for tokens

--- calima/src/token.rs
+++ calima/src/token.rs
@@ -50,7 +50,46 @@ pub enum Token<'input> {
 
 impl<'input> Display for Token<'input> {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-        write!(f, "{:?}", self)
+        match self {
+            Token::CurlyBraceOpen => write!(f, "{{"),
+            Token::CurlyBraceClose => write!(f, "}}"),
+            Token::ParenOpen => write!(f, "("),
+            Token::ParenClose => write!(f, ")"),
+            Token::SquareBracketOpen => write!(f, "["),
+            Token::SquareBracketClose => write!(f, "]"),
+            Token::Period => write!(f, "."),
+            Token::Comma => write!(f, ","),
+            Token::NameIdentifier(id) => write!(f, "{}", id),
+            Token::TypeIdentifier(id) => write!(f, "{}", id),
+            Token::OperatorIdentifier(id) => write!(f, "{}", id),
+            Token::StringLiteral(lit) => write!(f, "{}", lit),
+            Token::NumberLiteral((lit , _)) => write!(f, "{}", lit),
+            Token::BooleanLiteral(bool) => write!(f, "{}", bool),
+            Token::Do => write!(f, "do"),
+            Token::Let => write!(f, "let"),
+            Token::Rec => write!(f, "rec"),
+            Token::Underscore => write!(f, "_"),
+            Token::In => write!(f, "in"),
+            Token::Fun => write!(f, "fun"),
+            Token::If => write!(f, "if"),
+            Token::Then => write!(f, "then"),
+            Token::Else => write!(f, "else"),
+            Token::Case => write!(f, "case"),
+            Token::Of => write!(f, "of"),
+            Token::End => write!(f, "end"),
+            Token::Type => write!(f, "type"),
+            Token::Region => write!(f, "region"),
+            Token::Equal => write!(f, "="),
+            Token::Pipe => write!(f, "|"),
+            Token::Import => write!(f, "import"),
+            Token::Colon => write!(f, ":"),
+            Token::Arrow => write!(f, "->"),
+            Token::Backtick => write!(f, "`"),
+            Token::At => write!(f, "@"),
+            Token::Apostrophe => write!(f, "'"),
+            Token::Infix => write!(f, "infix"),
+            Token::Prefix => write!(f, "prefix")
+        }
     }
 }
 
