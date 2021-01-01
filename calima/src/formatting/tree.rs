fc67b09000c27d5e9756ac43164c618058768f77
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-01-01 18:40:26 +0100

    Add format helper for children and references

--- calima/src/formatting/tree.rs
+++ calima/src/formatting/tree.rs
@@ -9,3 +9,23 @@ pub trait TreeFormat {
         }
     }
 }
+
+impl<C> TreeFormat for &C {
+    fn get_precedence(&self) -> i32 {
+        self.get_precedence()
+    }
+
+    fn format(&self) -> String {
+        self.format()
+    }
+}
+
+pub fn format_children<R: TreeFormat, C: TreeFormat, I: Iterator<Item = C>>(
+    root: &R,
+    iter: I,
+    sep: &str,
+) -> String {
+    iter.map(|e| root.format_child(e))
+        .collect::<Vec<String>>()
+        .join(sep)
+}
