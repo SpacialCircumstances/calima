c2edd1c1db80433c6a41ad416b98ea0360b551b4
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-01-01 23:05:35 +0100

    Fix stack overflow caused by faulty trait implementation

--- calima/src/formatting/tree.rs
+++ calima/src/formatting/tree.rs
@@ -1,7 +1,7 @@
 pub trait TreeFormat {
     fn get_precedence(&self) -> i32;
     fn format(&self) -> String;
-    fn format_child<C: TreeFormat>(&self, child: C) -> String {
+    fn format_child<C: TreeFormat>(&self, child: &C) -> String {
         if child.get_precedence() >= self.get_precedence() {
             format!("({})", child.format())
         } else {
@@ -10,17 +10,7 @@ pub trait TreeFormat {
     }
 }
 
-impl<C> TreeFormat for &C {
-    fn get_precedence(&self) -> i32 {
-        self.get_precedence()
-    }
-
-    fn format(&self) -> String {
-        self.format()
-    }
-}
-
-pub fn format_children<R: TreeFormat, C: TreeFormat, I: Iterator<Item = C>>(
+pub fn format_children<'a, R: TreeFormat, C: TreeFormat + 'a, I: Iterator<Item = &'a C>>(
     root: &R,
     iter: I,
     sep: &str,
