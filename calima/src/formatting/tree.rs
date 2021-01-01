7537336d80be21e90d3591b84a4367ff0cd5e173
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-01-01 18:33:29 +0100

    Default formatting with parens

--- calima/src/formatting/tree.rs
+++ calima/src/formatting/tree.rs
@@ -1,5 +1,11 @@
 pub trait TreeFormat {
     fn get_precedence(&self) -> i32;
     fn format(&self) -> String;
-    fn format_child<C: TreeFormat>(&self, child: C) -> String;
+    fn format_child<C: TreeFormat>(&self, child: C) -> String {
+        if child.get_precedence() >= self.get_precedence() {
+            format!("({})", child.format())
+        } else {
+            child.format()
+        }
+    }
 }
