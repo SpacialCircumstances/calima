0e8711c19d331d1a7cacc19317ff16e65711675e
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-01-02 12:55:26 +0100

    Fix precedence in tree formatting to ensure function types are printed correctly

--- calima/src/formatting/tree.rs
+++ calima/src/formatting/tree.rs
@@ -2,7 +2,7 @@ pub trait TreeFormat {
     fn get_precedence(&self) -> i32;
     fn format(&self) -> String;
     fn format_child<C: TreeFormat>(&self, child: &C) -> String {
-        if child.get_precedence() >= self.get_precedence() {
+        if self.get_precedence() < child.get_precedence() {
             format!("({})", child.format())
         } else {
             child.format()
