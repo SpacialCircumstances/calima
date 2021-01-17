00c20daf9737552e93134d7f6d2a0f6535ddbb56
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-01-17 13:52:45 +0100

    Add methods for inserting things into a module environment

--- calima/src/typechecker/env.rs
+++ calima/src/typechecker/env.rs
@@ -24,6 +24,20 @@ impl<'a> ModuleEnvironment<'a> {
     }
 }
 
+impl<'a> ModuleEnvironment<'a> {
+    pub fn add_value(&mut self, name: &'a str, sch: Scheme) {
+        self.names.insert(name, sch);
+    }
+
+    pub fn add_operator(&mut self, name: &'a str, sch: Scheme, ops: OperatorSpecification) {
+        self.operators.insert(name, (sch, ops));
+    }
+
+    pub fn add_module(&mut self, name: &'a str, module: Box<dyn Environment>) {
+        self.modules.insert(name, module);
+    }
+}
+
 impl<'a> Environment for ModuleEnvironment<'a> {
     fn lookup_name(&self, name: &str) -> Option<&Scheme> {
         self.names.get(name)
