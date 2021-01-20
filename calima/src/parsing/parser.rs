5d68e05aeb035d04574c97e83970e05234b8bc34
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2021-01-20 23:04:37 +0100

    Add function for parsing type strings into type annotations

--- calima/src/parsing/parser.rs
+++ calima/src/parsing/parser.rs
@@ -10,6 +10,21 @@ lalrpop_mod!(
     "/parsing/calima_parser.rs"
 );
 
+pub fn parse_type<'source, 'input>(
+    text: &'source str,
+    interner: &'input StringInterner,
+) -> Result<TypeAnnotation<'input, Span>, ParseError<Location, Token<'input>, Error>> {
+    let lexer = Lexer::new(text, interner);
+    let parser = calima_parser::TypeAnnotation0Parser::new();
+    parser.parse(
+        &|left_loc, right_loc| Span {
+            left: left_loc,
+            right: right_loc,
+        },
+        lexer,
+    )
+}
+
 pub fn parse<'source, 'input>(
     code: &'source str,
     interner: &'input StringInterner,
