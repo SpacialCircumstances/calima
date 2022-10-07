97fdf302f31ad493cd75c8aa1df71c10ed081aa8
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2022-10-07 16:03:09 +0200

    Re-add basic cli

--- calimac/src/main.rs
+++ calimac/src/main.rs
@@ -1,3 +1,19 @@
+use calima::{compile, CompilerArguments};
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author = "SpacialCircumstances", version = "0.2.0")]
+struct Options {
+    #[arg(short, long, value_name = "Main file")]
+    main_file: String,
+    #[arg(short, long, value_name = "Module roots")]
+    roots: Vec<String>,
+    #[arg(short, long, value_name = "Project name")]
+    project_name: Option<String>,
+}
+
 fn main() -> Result<(), ()> {
-    todo!()
+    let args = Options::parse();
+
+    compile(CompilerArguments::new(&args.main_file, &args.roots, None))
 }
